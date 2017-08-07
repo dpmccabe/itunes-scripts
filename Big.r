98260aa7d0b06db3e935d5ec5cee9bebb13d4e01
Devin McCabe <devin.mccabe@gmail.com>
2017-08-07 18:02:37 -0400

    min n_artists

--- Big.r
+++ Big.r
@@ -22,12 +22,12 @@ com_tracks <- bind_rows(ungrouped_tracks, grouped_tracks)
 artist_counts <- com_tracks %>% group_by(uuid, artist) %>% count() %>%
   ungroup() %>% select(-n) %>% count(artist)
 
-com_tracks <- com_tracks %>% semi_join(artist_counts %>% filter(n > 0), by = "artist")
+com_tracks <- com_tracks %>% semi_join(artist_counts %>% filter(n >= 2), by = "artist")
 
 sizes <- com_tracks %>%
   group_by(uuid) %>% summarize(size = sum(size)) %>%
   arrange(-size)
 
-bigs <- left_join(com_tracks, sizes[1:50,], by = "uuid") %>% arrange(-size.y)
+bigs <- left_join(com_tracks, sizes[1:20,], by = "uuid") %>% arrange(-size.y)
 
 write(paste0(c(bigs$id), collapse = ","), file = "~/Music/big_ids.txt")
